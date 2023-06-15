77e12b8383f9b5b7df67e4836d6e5c020ecb50f2
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-15 15:30:24 -0700

    Fifth commit. First c code. Fourth commit failed..and so did the fifth. This is the sixth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 
 int main(void)
-{
+/* Comment for "main" function
+ * Prints a string unto the screen.
+ */
+	{
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

79ef7d20c3879276d0a4063462f861d46ce7e397
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-19 17:43:11 -0700

    Commit for second advanced task.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - prints all possible different combinations,
+  * of two digits.
+  *
+  * Return: returns 0
+  */
+
+int main(void)
+{
+	int firstdigit;
+	int secdigit;
+	int thrdigit;
+
+	for (firstdigit = 0; firstdigit <= 7; firstdigit++)
+	{
+		for (secdigit = firstdigit + 1; secdigit <= 8; secdigit++)
+		{
+			for (thrdigit = secdigit + 1; thrdigit <= 9; thrdigit++)
+			{
+				putchar(firstdigit + '0');
+				putchar(secdigit + '0');
+				putchar(thrdigit + '0');
+				if (!((firstdigit == 7) && (secdigit == 8) && (thrdigit == 9)))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

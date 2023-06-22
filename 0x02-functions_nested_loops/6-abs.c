77c0baa2860dca582c98902ebf26ecb3db777143
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-21 19:55:50 -0700

    commit for task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _abs- computes the absolute value of an integer
+  * @int: integer parameter.
+  * Return: Always 0
+  */
+
+int _abs(int)
+{
+	_putchar(abs(int));
+	return (0);
+}

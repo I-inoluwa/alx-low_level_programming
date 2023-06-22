e77cb5d9db5d1563388858b1c3d26cc331d2e4f0
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-21 21:29:59 -0700

    Re: Fixed all failed checks.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,30 @@ void print_to_98(int n)
 {
 	int num;
 
-	for (num = n; num <= 98; num++)
+	if (n < 98)
 	{
-		_putchar((num / 10) + '0');
-		_putchar((num % 10) + '0');
-		if (num != 98)
+		for (num = n; num <= 98; num++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+			if (num != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (num = n; num >= 98; num--)
+		{
+			_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+			if (num != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	_putchar('\n');

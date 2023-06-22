3c3aee2ad5e0ce010cb72e946e5d9e7b59ac4a99
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-21 20:06:53 -0700

    Commit for eight task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_last_digit - prints the last figit of a number.
+  * @num: number to be analysed
+  * Return: returns the last digit.
+  */
+
+int print_last_digit(num)
+{
+	return (num % 10);
+}

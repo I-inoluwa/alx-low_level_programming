ee1dcfd4df20353d8095de2f73905af460247d66
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-21 20:58:34 -0700

    Commit for last mandatory task.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_to_98 - prints all natural numbers from 'n' to 98.
+  * @n: start of range of numbers
+  * Return: returns nothing
+  */
+
+void print_to_98(int n)
+{
+	int num;
+
+	for (num = n; num <= 98; num++)
+	{
+		_putchar(num + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}

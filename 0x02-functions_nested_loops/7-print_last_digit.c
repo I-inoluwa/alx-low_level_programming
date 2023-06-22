633aa80a972dffbf6abe48852b7fa579430a6075
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-21 20:39:20 -0700

    Re - commit for eight task.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,15 @@ int print_last_digit(int num)
 {
 	int last;
 
-	last = num % 10
+	last = num % 10;
 	if (num >= 0)
 	{
 		putchar(last + '0');
 	}
 	else
 	{
-		last = (-1) * last
+		last = (-1) * last;
 		putchar(last + '0');
 	}
-	return (last)
+	return (last);
 }

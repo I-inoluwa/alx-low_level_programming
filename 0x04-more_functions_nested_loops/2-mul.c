de56f716ac37140395ec343b00a7158886138b37
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-22 16:57:46 -0700

    Sixth commit, third task.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - multiplies two integers.
+  * @a: first integer to be multiplied;
+  * @b: second integer to be multiplied;
+  * Return: returns the result.
+  */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}

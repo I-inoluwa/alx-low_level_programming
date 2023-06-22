4e834589aa84019c2529dde4cf34967c6d2a978a
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-21 20:25:15 -0700

    commit for ninth task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * jack_bauer - prints every minute of the day of Jack Bauer
+  * from 00:00 to 23:59
+  *
+  * Return: returns nothing
+  */
+
+void jack_bauer(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 24; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 60; num2++)
+		{
+			_putchar((num1 / 10) + '0');
+			_putchar((num1 % 10) + '0');
+			_putchar(':');
+			_putchar((num2 / 10) + '0');
+			_putchar((num2 % 10) + '0');
+		}
+	}
+}

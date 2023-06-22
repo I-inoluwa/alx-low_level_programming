9999c26750570fe3d56c5a08146300f522368a5e
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-06-21 20:32:57 -0700

    commit for tenth task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * times_table - prints the 9-times table, starting with 0
+  * Return: returns nothing
+  */
+
+void times_table(void)
+{
+	int product;
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			product = num1 * num2;
+			_putchar(product + '0');
+		}
+	}
+}

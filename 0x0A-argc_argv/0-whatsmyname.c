9d9d79030a5fb6a232feab2ce2ec01b86ab21997
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-07 18:40:26 -0700

    First commit after ove a week. Whew!

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - takes no arguments, and prints the name of the program.
+  * @argc: int variable counting the number of arguments passed.
+  * @argv: a list containing all the arguments passed.
+  * Return: returns 0;
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+I really need to get back on track.
+ README for command line arguments in C

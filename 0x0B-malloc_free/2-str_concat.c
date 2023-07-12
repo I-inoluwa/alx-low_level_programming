a958756282aaf12065f710bf7097bb5457de387e
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-11 21:32:43 -0700

    Another commit.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,8 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, j;
+	int i, j, strlen;
 
-	str = malloc((sizeof(s1) + sizeof(s2)) - sizeof(char));
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -22,6 +21,13 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
+
+	strlen = 0;
+	for (i = 0; s1[i] || s2[i]; i++)
+		strlen++;
+
+	str = malloc(sizeof(char) * strlen);
+
 	if (str == NULL)
 	{
 		return (NULL);
@@ -30,21 +36,17 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	while (*(s1 + i))
 	{
-		*(str + i) = s1[i];
+		*(str + i) = *(s1 + i);
 		i++;
 	}
 	j = 0;
 	while (*(s2 + j))
 	{
-		*(str + i) = s2[j];
+		*(str + i) = *(s2 + j);
 		j++;
 		i++;
 	}
 
 	*(str + i) = '\0';
-	/*
-	 *free(str);
-	 */
 	return (str);
 }
-

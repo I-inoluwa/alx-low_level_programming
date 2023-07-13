e42aff7bf9ad8b66552617a62a298d575c6f9731
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-12 21:01:21 -0700

    I hope this works.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,18 +2,13 @@
 #include <stdlib.h>
 
 /**
- * strtow - splits a string into words
- * @str: string to be splitted string to be copied.
- * Return: returns an array of strings or NULL.
+ * get_arr_len - gets the height of a 2D array of strs.
+ * @str: string passed.
+ * Return: returns an integer.
  */
-
-char **strtow(char *str)
+int get_arr_len(char *str)
 {
-	char **arr;
-	int i, j, width, height, ref;
-
-	if ((str == NULL) || (*str == '\0'))
-		return (NULL);
+	int i, j, height;
 
 	height = 1;
 	i = 0;
@@ -31,26 +26,62 @@ char **strtow(char *str)
 		i++;
 	}
 
+	return (height);
+}
+
+
+/**
+ * get_str_len - gets the length of a string.
+ * @str: string passed as argument.
+ * @i: position of a char in str.
+ * Return: returns an integer.
+ */
+int *get_str_len(char *str, int i)
+{
+	int width;
+	int *arr;
+
+	arr = malloc(sizeof(int) * 2);
+
+	width = 0;
+	while ((str[i] != ' ') && (str[i]))
+	{
+		width++;
+		i++;
+	}
+	while (str[i] == ' ')
+		i++;
+
+	arr[0] = width;
+	arr[1] = i;
+
+	return (arr);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: strng to be splitted string to be copied.
+ * Return: returns an array of strings. or NULL.
+ */
+char **strtow(char *str)
+{
+	char **arr;
+	int *int_arr;
+	int i, j, width, height, ref;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	height = get_arr_len(str);
 	arr = malloc((sizeof(char *)) * (height + 1));
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
 
-	i = 0;
-	height = 0;
-	ref = 0;
+	i = 0, height = 0, ref = 0;
 	while (str[i])
 	{
-		width = 0;
-		while ((str[i] != ' ') && (str[i]))
-		{
-			width++;
-			i++;
-		}
-		while (str[i] == ' ')
-			i++;
-
+		int_arr = get_str_len(str, i);
+		width = int_arr[0];
+		i = int_arr[1];
 		if (width == 0)
 		{
 			ref = i;
@@ -60,14 +91,8 @@ char **strtow(char *str)
 		}
 		*(arr + height) = malloc((sizeof(char)) * (width + 1));
 		if (*(arr + height) == NULL)
-		{
-			for (; height >= 0; height--)
-			{
-				free(arr[height]);
-			}
-			free(arr);
 			return (NULL);
-		}
+
 		for (j = 0; j < width; j++)
 			arr[height][j] = str[ref + j];
 

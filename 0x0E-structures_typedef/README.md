2b19db5c2689c3253b915fea489040b827e73968
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-17 16:32:43 -0700

    First commit for the project.

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+README file for struct and typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - A new type, dog.
+  * @name: name of dog;
+  * @age: How old the dog is;
+  * @owner: owner of the dog.
+  *
+  * Description: This creates a new datatype of dog.
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+

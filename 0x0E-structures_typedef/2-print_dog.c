48d36bf1a6015c287156b20dec693bacc541ab94
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-17 18:22:21 -0700

    I hope this works.

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * print_dog - function tha tprints struct dog.
+  * @d: struct dog datatype.
+  * Return: returns void;
+  */
+
+void print_dog(struct dog *n)
+{
+	if (n->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", n->name);
+	}
+
+	if (n->age == NULL)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %.6f\n", n->age);
+	}
+
+	if (n->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", n->owner);
+	}
+
+}

b9ac9eb9ff526027f3b500a2b96f2c3ba6b83cfa
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-17 17:12:53 -0700

    Okay, now this is getting annoying.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,8 +13,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (1);
+	/*
+	   *if (d == NULL)
+	*	return (1);
+	*/
 
 	d->name = name;
 	d->age = age;

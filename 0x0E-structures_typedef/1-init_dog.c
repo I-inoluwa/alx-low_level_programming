f8d8f471a9543c37f5b6b8f93aaf64fc4b38854f
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-17 17:06:33 -0700

    solved the issue. Commit 8/

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,12 +11,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog tmp = { name, age, owner };
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (1);
 
-	d = &tmp;
-	/*
-	*d->name = name;
-	*d->age = age;
-	*d->owner = owner;
-	*/
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
 }

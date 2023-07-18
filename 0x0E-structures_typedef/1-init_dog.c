c590ee3c4edab8ca72dd340e49d898fbb7148977
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-17 17:29:40 -0700

    commit11/

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,10 +13,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
-	/*
-	   *if (d == NULL)
-	*	return (1);
-	*/
+	if (d == NULL)
+		return (NULL);
 
 	d->name = name;
 	d->age = age;

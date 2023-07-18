681c364f6835792e21ff6b08ea52de04279df9b6
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-17 20:22:16 -0700

    Final commit for the day.

---
+++ 0x0E-structures_typedef/5-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    dog_t *my_dog;
+
+    my_dog = new_dog("Poppy", 3.5, "Bobionjk");
+    printf("My name is %s, and I am %.1f :) - Woof! The name of my owner is %s...\n", my_dog->name, my_dog->age, my_dog->owner);
+    free_dog(my_dog);
+
+    return (0);
+}

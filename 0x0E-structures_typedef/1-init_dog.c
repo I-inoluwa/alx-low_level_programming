1d0528da5aeb4a8a2ea97f3bf5518417ca6e29a5
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-17 18:06:30 -0700

    WHew~!

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,15 +17,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d = malloc(sizeof(struct dog));
 	if (d != NULL)
 	{
-		for (i = 0; name[i] != '\0'; i++)
-			d->(*(name + i)) = name[i];
-		d->(*(name + i)) = '\0';
+		(*d).name = name;
 		d->age = age;
+		(*d).owner = owner;
 
-		for (i = 0; owner[i] != '\0'; i++)
-			d->(*(owner + i)) = owner[i];
-		d->owner(*(owner + i)) = '\0';
+		free(d);
 	}
-
-	free(d);
 }

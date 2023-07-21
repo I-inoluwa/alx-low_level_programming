bba77f2948c338fd9dc070b72c80a564aff39194
root <root@Ajileye>
2023-07-21 17:45:16 +0100

    Should be fixed now.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,15 +18,15 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	if (s == NULL)
-		return (NULL);
-
-	for (i = 0; ops[i].op != NULL; i++)
+	i = 0;
+	while ((ops[i].op != NULL) && (s != NULL))
 	{
 		if (((ops[i].op)[0] == s[0]) && (s[1] == '\0'))
 		{
 			return (ops[i].f);
 		}
+
+		i++;
 	}
 
 	return (NULL);

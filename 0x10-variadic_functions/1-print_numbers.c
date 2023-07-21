7409ab839d9a3e105efad3d35fedfdaad1bac471
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-07-20 18:36:30 -0700

    rectifying task 1.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,18 +10,19 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i, num;
+	int num;
+	unsigned int i;
 	va_list num_args;
 
 	if (n == 0)
 		return;
 
 	va_start(num_args, n);
-	for (i = 0; i < (int)n; i++)
+	for (i = 0; i < n; i++)
 	{
 		num = va_arg(num_args, int);
 		printf("%d", num);
-		if ((separator != NULL) && (i != ((int)n) - 1))
+		if ((separator != NULL) && (i != n - 1))
 			printf("%s", separator);
 	}
 	va_end(num_args);

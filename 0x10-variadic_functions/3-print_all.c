e2602a51d015d1aa119c0889e62354da3a09ac36
root <root@Ajileye>
2023-07-21 14:49:29 +0100

    Test commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,37 +15,37 @@ void print_all(const char * const format, ...)
 	unsigned int n = 0;
 
 	va_start(args, format);
-	if (format != NULL)
-	{
-		while (format[n] != '\0')
+	if (format == NULL)
 		{
-			fmt = format[n];
-			while (fmt == 'c' || fmt == 'f' || fmt == 'i' || fmt == 's')
-			{
-				printf("%s", sep);
-				switch (format[n])
-				{
-					case 'c':
-						printf("%c", va_arg(args, int));
-						break;
-					case 's':
-						each = va_arg(args, char *);
-						if (each == NULL)
-							each = "(nil)";
-						printf("%s", each);
-						break;
-					case 'i':
-						printf("%d", va_arg(args, int));
-						break;
-					case 'f':
-						printf("%f", va_arg(args, double));
-						break;
-				}
-				sep = ", ";
-				break;
-			}
-			n++;
+			printf("%c", format[0]);
 		}
+	while (format[n] != '\0')
+	{
+		fmt = format[n];
+		if (fmt == 'c' || fmt == 'f' || fmt == 'i' || fmt == 's')
+		{
+			printf("%s", sep);
+			switch (format[n])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 's':
+					each = va_arg(args, char *);
+					if (each == NULL)
+						each = "(nil)";
+					printf("%s", each);
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+			}
+			sep = ", ";
+		}
+		n++;
 	}
 	va_end(args);
 	printf("\n");

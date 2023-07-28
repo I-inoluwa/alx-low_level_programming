f9c0d583492738b123ecf9c5a9c85d6d9547af4e
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-07-28 02:55:21 +0100

    First commit for the task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints every item of a linked list.
+ * @h: linked list head item.
+ * Return: the size of the linked list;
+*/
+size_t print_list(const list_t *h)
+{
+	size_t len;
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (-1);
+	}
+
+	len = 0;
+	tmp->str = h->str;
+	tmp->len = h->len;
+	tmp->next = h->next;
+
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+			tmp->str = "(nil)";
+
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		len++;
+		tmp = tmp->next;
+	}
+	free(tmp);
+
+	return (len);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+README for singly linked lists.
+Hopefully, I get 200% in this.

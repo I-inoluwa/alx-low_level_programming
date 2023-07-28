2239d7fdfbf73679e967aade3c2fefb018a7ff7a
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-07-28 20:38:34 +0100

    Finally.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,47 +3,23 @@
 
 /**
  * print_message - prints a message.
- * Return: returns an int;
+ * Return: returns void;
 */
 
-int print_message(void)
+void print_message(void)
 {
 	printf(
 		"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
 		);
-	return (1);
-}
 
+}
 
 /**
  * before_main - prints a message before main;
- * Return: returns (0);
+ * Return: returns void;
 */
 
-int before_main(void)
+void before_main(void)
 {
-	func_node *node1, *node2, *tmp;
-
-	node1 = malloc(sizeof(func_node));
-	node2 = malloc(sizeof(func_node));
-
-	if (node1 == NULL || node2 == NULL)
-		return (-1);
-
-	node1->main_ptr = print_message;
-	node1->next = node2;
-	node2->main_ptr = main;
-	node2->next = NULL;
-
-	tmp = node1;
-	while (tmp != NULL)
-	{
-		tmp->main_ptr();
-		tmp = tmp->next;
-	}
-	free(node2);
-	free(node1);
-	exit(100);
-
-	return (0);
+	print_message();
 }

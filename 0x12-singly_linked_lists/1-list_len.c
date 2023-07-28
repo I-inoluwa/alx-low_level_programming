2b8026f2c914a77dc269b1d1798f2e8d90b997fa
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-07-28 03:14:11 +0100

    second commit.

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list.
+ * @h: linked list argument passed;
+ * Return: length of the linked list.
+*/
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (-1);
+	}
+
+	tmp->str = h->str;
+	tmp->len = h->len;
+	tmp->next = h->next;
+
+	if (tmp == NULL)
+		return (1);
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+	free(tmp);
+	return (len);
+}

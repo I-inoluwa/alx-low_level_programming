05e10d6dc02af1edaecbedc0c219e128b078e7cf
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-07-29 00:50:04 +0100

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - prints a string by recursion;
+ * @s: String to be printed.
+ * Return: returns void;
+*/
+
 void _puts_recursion(char *s)
 {
 	if (s == NULL)
 		return;
-	
+
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
-	_puts_recursion(&(*(s+1)));
+	_puts_recursion(&(*(s + 1)));
 }

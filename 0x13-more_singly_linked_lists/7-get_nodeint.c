bf984d80d8568a1b82bee7764e74f0a32fb3c0bd
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-08-01 11:00:46 +0100

    Incomplete save

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp = head;
 	int i = 0;
 
+	if (head == NULL)
+		return (0);
+
 	while (i < index)
 	{
 		head = tmp->next;
 		tmp = head;
+
+		if (tmp == NULL)
+			return (0);
 	}
 
 	return (tmp);

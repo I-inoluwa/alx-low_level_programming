28bfc0b34b6677a17c5d33863ed480d847103311
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-08-01 02:01:07 +0100

    Sixth commit.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list and sets head to NULL.
+ * @head: pointer to the linked list to be freed;
+ * Return: returns void;
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	if ((*head) == NULL)
+		return;
+
+	while ((*head) != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	*head = NULL;
+}

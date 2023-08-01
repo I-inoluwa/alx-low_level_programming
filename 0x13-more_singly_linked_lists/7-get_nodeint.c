450b2538a1d3753371a01951e2817e0e94a1cf11
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-08-01 10:55:50 +0100

    Incomplete sve

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * 
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp = head;
+	int i = 0;
+
+	while (i < index)
+	{
+		head 
+	}
+
+	return (tmp);
+	
+
+}

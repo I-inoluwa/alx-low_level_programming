4a30f5c65fdcf94208ff286d5d2251f49b629958
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-08-01 14:53:59 -0700

    Moved from my local termial to sandbox again. COmmit for task 8.

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 
 /**
- * 
-*/
+ * sum_listint - takes the sum of the value of all nodes;
+ * @head: linked list;
+ * Return: returns sum, or (0), if list is empty.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *tmp = head;
+
+	if (head == NULL)
+		return (0);
+
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		head = tmp->next;
+		tmp = head;
+	}
+
+	return (sum);
+}

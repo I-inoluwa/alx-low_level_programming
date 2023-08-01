b11d914df8e5ee69ef5574b1d46f59c84a341b58
I-inoluwa <Ajileyeolaoluwa@yahoo.com>
2023-08-01 10:45:11 +0100

    Seems like there re some forces against me.

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops the head element of a linked list;
+ * @head: head node;
+ * Return: returns the value of the node popped;
+*/
+
+int pop_listint(listint_t **head)
+{
+	int cache;
+	listint_t *tmp = *head;
+
+	if (head == NULL)
+		return (0);
+
+	*head = tmp->next;
+	cache = tmp->n;
+	free(tmp);
+
+	return (cache);
+}

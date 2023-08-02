fc184559b73c69c0eef8fce1f50bafc67f18918f
Olaoluwa <ajileyeolaoluwa@yahoo.com>
2023-08-01 17:44:58 -0700

    Commit for first advanced task.

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - reverses a linked list;
+  * @head: pointer to linked list to be reversed;
+  * Return: returns the address of the first node of reversed list;
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp, *current;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	tmp = (*head)->next;
+	(*head)->next = NULL;
+
+	while (tmp != NULL)
+	{
+		current = tmp->next;
+		tmp->next = *head;
+		*head = tmp;
+		tmp = current;
+	}
+
+	return (tmp);
+}

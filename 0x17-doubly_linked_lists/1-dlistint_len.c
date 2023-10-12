8e6e5e5195dfc29dd042d31c15d22c5d1101f09b
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-13 00:57:01 +0100

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+
+/**
+ * dlistint_len - gets the length of a doubly linked list
+ * @h: head of the linked list;
+ * Return: returns number of elements.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}

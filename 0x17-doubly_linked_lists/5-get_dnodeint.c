a9fd869b36c51c69ff41258c86d1cb0ebe2b32a8
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-13 01:28:45 +0100

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,25 @@
 #include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets the node at a specified index;
+ * @head: head node of the doubly linked list
+ * @index: specified index;
+ * Return: returns a node, or NULL on failure;
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; head != NULL; i++)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+	}
+
+	return (NULL);
+}

6c1cf842d79194e8e7db55b8d9c4304ee9addfed
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 18:16:32 +0100

    3-hash_table_set.ca

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL, *head = NULL;
 	unsigned long int hash = 0;
 
-	if (key == NULL || key[0] == '\0' || value == NULL)
+	if (key == NULL || key[0] == '\0' || value == NULL || ht == NULL)
 		return (0);
 
 	new = malloc(sizeof(hash_node_t));

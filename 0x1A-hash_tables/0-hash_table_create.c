1d740f6d3394ff12eb9c42827058ff4f91c0ca57
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 16:15:10 +0100

    Commit 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new = NULL;
 	hash_node_t **arr = NULL;
 
-	if (size <= 0)
-		return (NULL);
-
 	new = malloc(sizeof(hash_table_t));
 	arr = malloc(sizeof(hash_node_t) * (size + 1));
 	if (new == NULL || arr == NULL)
@@ -24,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	new->size = size;
 	new->array = arr;
-	(new->array)[size] = NULL;
+	/*(new->array)[size] = NULL;*/
 
 	return (new);
 }

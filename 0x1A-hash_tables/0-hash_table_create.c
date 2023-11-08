72d4ceeee4d91456f0f1538eb65e9b9faf5bd35f
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-11-08 16:10:22 +0100

    Commit 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates an hash table of size 'size';
+ * @size: size of the hash table to be created;
+ * Return: returns the address to the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = NULL;
+	hash_node_t **arr = NULL;
+
+	if (size <= 0)
+		return (NULL);
+
+	new = malloc(sizeof(hash_table_t));
+	arr = malloc(sizeof(hash_node_t) * (size + 1));
+	if (new == NULL || arr == NULL)
+	{
+		free(new);
+		free(arr);
+		return (NULL);
+	}
+	new->size = size;
+	new->array = arr;
+	(new->array)[size] = NULL;
+
+	return (new);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,5 @@
+
+Been a while I wrote a code in C.
+
+README file for projects on hash tables. This should be interesting.
+
